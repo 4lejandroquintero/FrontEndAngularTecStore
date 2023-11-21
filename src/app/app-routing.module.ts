adba21bb30f66018620f0dfaf59701e1d3554815
4lejandroquintero <alejoq96@gmail.com>
2023-11-21 16:47:08 -0500

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,52 @@
+import { NormalGuard } from './services/normal.guard';
+import { AdminGuard } from './services/admin.guard';
+import { UserDashboardComponent } from './pages/user/user-dashboard/user-dashboard.component';
+import { DashboardComponent } from './pages/admin/dashboard/dashboard.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ProductoListaComponent } from './pages/producto-lista/producto-lista.component';
+import { AgregarProductoComponent } from './pages/agregar-producto/agregar-producto.component';
+import { EditarProductoComponent } from './pages/editar-producto/editar-producto.component';
+
+const routes: Routes = [
+  {
+    path : '',
+    component : HomeComponent,
+    pathMatch : 'full'
+  },
+  {
+    path : 'signup',
+    component : SignupComponent,
+    pathMatch : 'full'
+  },
+  {
+    path : 'login',
+    component : LoginComponent,
+    pathMatch : 'full'
+  },
+  {
+    path:'admin',
+    component:DashboardComponent,
+    pathMatch:'full',
+    canActivate:[AdminGuard]
+  },
+  {
+    path:'user-dashboard',
+    component:UserDashboardComponent,
+    pathMatch:'full',
+    canActivate:[NormalGuard]
+  },
+  {path:'productos', component: ProductoListaComponent},
+  {path: '', redirectTo: 'productos', pathMatch:'full'},
+  {path: 'agregar-producto', component: AgregarProductoComponent},
+  {path: 'editar-producto/:id', component: EditarProductoComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/services/producto.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import baserUrl from './helper';
+import { Producto } from './producto';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductoService {
+
+
+  constructor(private clienteHttp: HttpClient) { }
+
+  obtenerProductosLista(): Observable<Producto[]>{
+    return this.clienteHttp.get<Producto[]>(`${baserUrl}/inventario-app/productos`);
+  }
+
+  agregarProducto(producto: Producto): Observable<Object>{
+    return this.clienteHttp.post(`${baserUrl}/inventario-app/productos`, producto);
+ }
+
+ obtenerPorudctoPorId(id: number){
+  return this.clienteHttp.get<Producto>(`${baserUrl}/inventario-app/productos/${id}`)
+ }
+
+ guardarrProducto(id: number, producto: Producto): Observable<Object>{
+  return this.clienteHttp.put(`${baserUrl}/inventario-app/productos/${id}`, producto);
+ }
+
+ eliminarProducto(id: number): Observable<Object>{
+  return this.clienteHttp.delete(`${baserUrl}/inventario-app/productos/${id}`);
+ }
+}
